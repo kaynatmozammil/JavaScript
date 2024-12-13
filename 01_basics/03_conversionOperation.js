3f029a090d1c6b629c94a29dbe08d0b272bb851d
Kaynat Mozammil <143025590+kaynatmozammil@users.noreply.github.com>
2024-12-13 20:05:09 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = false
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33kbc" => NaN
+//  true => 1 & false => 0
+// 
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "kaynat " => true
+
+let someNumber = 33
+console.log(typeof someNumber)
+let strNumber = String(someNumber)
+console.log(typeof strNumber)
