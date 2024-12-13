a64bce6367191ec264c9db753d3428ab87e1044e
Kaynat Mozammil <143025590+kaynatmozammil@users.noreply.github.com>
2024-12-13 20:18:59 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = false
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 // "33" => 33
@@ -15,7 +15,7 @@ console.log(valueInNumber)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true
 // 0 => false
@@ -23,6 +23,47 @@ console.log(booleanIsLoggedIn)
 // "kaynat " => true
 
 let someNumber = 33
-console.log(typeof someNumber)
+// console.log(typeof someNumber)
 let strNumber = String(someNumber)
-console.log(typeof strNumber)
+// console.log(typeof strNumber)
+
+
+// ******************** Operations *************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Kaynat"
+
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3)
+
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter
+console.log(gameCounter)
