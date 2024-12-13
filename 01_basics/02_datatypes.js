3206fcc55ca65262a9a6a776ddd62e2c7445df0a
Kaynat Mozammil <143025590+kaynatmozammil@users.noreply.github.com>
2024-12-13 19:50:41 +0000

    dataTypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version of js
+
+// alert(3+3) // We are using nodejs , not browser
+
+console.log(4 
+    +
+     2) // code readablity shoud be high
+
+console.log("Kaynat");
+
+
+let name = "Kaynat"
+let age = 30
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => Standalone value
+// undefind => np value assign
+// symbol => unique
+
+
+
+
+// object 
+
+console.log(typeof(undefined))
